b0f84085f9616fe829304460ac0766ffc1e96d7e
Benedito Lucas de Toledo <46333024+BeneditoLT@users.noreply.github.com>
2023-05-25 15:54:10 -0300

    Update app.js

--- server/scr/app.js
+++ server/scr/app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end('<h1>It Works!</h1>');
 });
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1;
 const port = 3000;
 
 server.listen(port, hostname, () => {
